82b5a98750bb8261944ab8db7e86904eae42b9ec
AnaelBerrouet <anael.jberrouet@gmail.com>
2018-11-06 18:19:34 +0000

    added exercise 2 & 3

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+#Store.find defaults to finding using the ID
+@store1 = Store.find(1)
+pp @store1
+@store2 = Store.find(2)
+
+@store1.name = "Guess Jeans"
+@store1.save
+
+pp @store1
