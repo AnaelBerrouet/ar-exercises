dbfc9cc48313d259fb55a98e27fe9f71dbaf4ac0
AnaelBerrouet <anael.jberrouet@gmail.com>
2018-11-06 22:22:17 +0000

    added exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -4,6 +4,14 @@ class Store < ActiveRecord::Base
   validates :name,
     length: { minimum: 3 }
   validates :annual_revenue,
-    numericality: { only_integer: true, greater_than: 0}
+    numericality: { only_integer: true, greater_than_or_equal_to: 0}
 
+  before_destroy :check_empty
+
+  private
+    def check_empty
+      if :employees.size > 0 && annual_revenue > 0
+        throw :abort
+      end
+    end
 end
